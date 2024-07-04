fda0fb13786367406d9a82d679607bbb8e0d5b55
Yuvrajsinh Jhala <png625@gmail.com>
2024-07-04 09:42:33 +0530

    Implementing customers page

--- src/pages/CustomersPage.vue
+++ src/pages/CustomersPage.vue
@@ -1,11 +1,11 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <ComingSoonView />
+      <CustomersDataTable />
     </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
-import ComingSoonView from '@/views/ComingSoonView.vue'
+import CustomersDataTable from '@/views/CustomersDataTableView.vue'
 </script>
---
+++ src/stores/customer.ts
@@ -0,0 +1,52 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import { ref } from 'vue'
+import { useAuthStore } from './auth'
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const useCustomerStore = defineStore('customer', () => {
+  const customers = ref([])
+  const totalItems = ref(0)
+  const loading = ref(false)
+
+  const authStore = useAuthStore()
+
+  const fetchCustomers = async (options: any = {}) => {
+    loading.value = true
+    const { page = 1, itemsPerPage = 5, sortBy = [], sortDesc = [], search = '' } = options
+    const sort = sortBy.length ? `${sortBy[0]} ${sortDesc[0] ? 'desc' : 'asc'}` : ''
+
+    try {
+      const response = await axios.get(`${API_BASE_URL}/admin/user-listing`, {
+        params: {
+          page,
+          limit: itemsPerPage,
+          sort,
+          search
+        },
+        headers: {
+          Authorization: `Bearer ${authStore.getToken}`
+        }
+      })
+
+      if (response.data && response.data.data) {
+        customers.value = response.data.data
+        totalItems.value = response.data.total
+      } else {
+        console.error('Unexpected API response structure:', response.data)
+      }
+    } catch (error) {
+      console.error('Error fetching customers:', error)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    customers,
+    totalItems,
+    loading,
+    fetchCustomers
+  }
+})
