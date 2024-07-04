5b1cf2261d2d8ddfefbd0fac129c2643c606d125
Yuvrajsinh Jhala <png625@gmail.com>
2024-07-04 21:58:16 +0530

    Added logo on header

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app-bar app color="primary" flat>
-    <v-toolbar-title>PetShop</v-toolbar-title>
+    <v-spacer />
+    <v-toolbar-title><router-link to="/"><img src="@/assets/logo.svg" alt="Logo" class="logo" height="25" /></router-link></v-toolbar-title>
     <v-spacer />
     <v-btn to="/products" class="mx-2" color="white"> PRODUCTS </v-btn>
     <v-btn to="/promotions" class="mx-2" color="white"> PROMOTIONS </v-btn>
@@ -8,6 +9,7 @@
     <v-spacer />
     <v-btn color="white"> <v-icon>mdi-cart</v-icon> Cart </v-btn>
     <v-btn color="white" @click="logout"> <v-icon>mdi-logout</v-icon> Logout </v-btn>
+    <v-spacer />
   </v-app-bar>
 </template>
 <script setup lang="ts">
