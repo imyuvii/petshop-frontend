239c386367cfd2c4db4dea20bd6a0bce1f3c8d07
Yuvrajsinh Jhala <png625@gmail.com>
2024-07-06 12:06:57 +0530

    added sample pages

---
+++ src/components/ExternalRedirect.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Redirecting...</div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const props = defineProps<{ url: string }>()
+const router = useRouter()
+
+onMounted(() => {
+  if (props.url) {
+    window.location.href = props.url
+  } else {
+    router.replace('/')
+  }
+})
+</script>
---
+++ src/pages/ComingSoonPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row>
+    <ComingSoonView />
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import ComingSoonView from '@/views/ComingSoonView.vue'
+</script>
