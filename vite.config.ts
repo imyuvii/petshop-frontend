376eb6c3a526a413f2eefb1fc474a048a145d802
Yuvrajsinh Jhala <png625@gmail.com>
2024-07-06 11:27:25 +0530

    Setting up vite for Vue test cases

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  test: {
+    deps: {
+      inline: ['vuetify']
+    }
   }
 })
